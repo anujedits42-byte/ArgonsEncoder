8c57bc46b7c2748b5f4efa888cea489c6a888ed9
anujedits42-byte <anujedits42@gmail.com>
2026-04-07 09:43:03 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,24 +7,24 @@ load_dotenv()
 
 
 # Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8564884831:AAF0ksogvIew4CDv5N8HcSJOq-vkCrrAOHk")
 # Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "12345678"))  # Placeholder ID
+APP_ID = int(os.environ.get("APP_ID", "34446649"))  # Placeholder ID
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
+API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 # Your db channel Id
-LOG_CHANNEL = int(os.environ.get("CHANNEL_ID", "-1001234567890"))  # Placeholder channel ID
+LOG_CHANNEL = int(os.environ.get("CHANNEL_ID", "-1003515041061"))  # Placeholder channel ID
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "YOUR_OWNER_USERNAME_OR_ID")
+OWNER = os.environ.get("OWNER", "7892805795")
 # OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1234567890"))  # Placeholder owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "7892805795"))  # Placeholder owner ID
 # Port
 PORT = os.environ.get("PORT", "8030")
 # Database
 DB_URI = os.environ.get(
     "DATABASE_URL",
-    "mongodb+srv://user:password@cluster0.abcde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",  # Placeholder DB URI
+    "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0",  # Placeholder DB URI
 )
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster")
+DB_NAME = os.environ.get("DATABASE_NAME", "Anujedit")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
